1c46b2a645acb0cbd8b3cbaad8e5296fe1ab827a
PedDavid <peddavid96@outlook.com>
2017-05-29 02:48:30 +0100

    Allow Jest to use Babel
    By extracting babel configuration (previously in wepack.config.js)
    to its own file (.babelrc) we allow Jest to use it.
    Import syntax wasn't working in tests because imports were handled by
    webpack instead of babel (with present env).
    Added babel preset env and object rest-spread syntax capability
    (this should be present in preset env but was not working).
    Added describe to standard globals.
    
    Env preset: http://babeljs.io/docs/plugins/preset-env/

--- WebClient/webpack.config.js
+++ WebClient/webpack.config.js
@@ -17,10 +17,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['react']
-          }
+          loader: 'babel-loader'
         }
       }
     ]
