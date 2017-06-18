4c162ca87c98ca77a93d5448960190a06d95ffba
Pedro Cipriano <a41890@alunos.isel.pt>
2017-06-18 19:17:03 +0100

    add repositories interfaces

---
+++ Server/API/Models/IRepositories/IImageRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models.IRepositories {
+    public interface IImageRepository : IFigureRepository<Image> {
+    }
+}
