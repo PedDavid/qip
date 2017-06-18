b7256cf730efd317e8ca49c5e9e766db5c4e957d
Pedro Cipriano <a41890@alunos.isel.pt>
2017-06-18 18:05:22 +0100

    add view of User_Board with all columns of User and Board

---
+++ Server/Database/dbo/Views/Full_User_Board.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[Full_User_Board]
+AS 
+	SELECT ub.permission, u.id AS userId, u.username, u.[name], b.id AS boardId, b.[name] AS boardName, b.maxDistPoints 
+	FROM dbo.User_Board AS ub 
+	INNER JOIN dbo.[User] AS u
+	ON (ub.userId = u.id)
+	INNER JOIN dbo.Board AS b
+	ON(ub.boardId = b.id)
