7a1fde238aa3e4388e7c4dbf6702ab628e2b8d8d
Pedro Cipriano <a41890@alunos.isel.pt>
2017-06-18 19:21:31 +0100

    add contract of api

---
+++ Server/API/Models/Input/InLineStyle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models.Input {
+    public class InLineStyle {
+        public long? Id { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Server/API/Models/Output/OutLineStyle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models.Output {
+    public class OutLineStyle {
+        public long Id { get; set; }
+        public string Color { get; set; }
+    }
+}
