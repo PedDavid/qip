8187c69c0c6ad806b9a445b82a9300f8b38039b6
Pedro Cipriano <a41890@alunos.isel.pt>
2017-06-18 18:08:28 +0100

    add functions to get specific information of db

---
+++ Server/Database/dbo/Functions/GetFigures.sql
@@ -0,0 +1,14 @@
+﻿create function dbo.GetFigures(@boardId bigint) returns table
+as
+	return(
+		select fig.boardId, fig.id, fig.figureType, lineX.isClosedForm, lineX.lineStyleId, lineX.pointId, lineX.pointStyleId,
+			img.initPointId, img.src, img.width, img.height 
+			from dbo.Figure as fig 
+				full outer join dbo.[Image] as img
+				on(img.figureId = fig.id and fig.boardId = 1)
+				full outer join 
+				(select lin.figureId, lin.boardId, lin.isClosedForm, lin.lineStyleId, figP.pointId, figP.pointStyleId from dbo.Line as lin
+					inner join dbo.Line_Point as figP
+					on (figP.figureId = lin.figureId and figP.boardId = @boardId) ) as lineX
+				on(lineX.figureId = fig.id and fig.boardId = @boardId)
+	)
