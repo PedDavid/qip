e7d672e0a96680969e0b0a494c0a423bfb25ac7f
Pedro Cipriano <a41890@alunos.isel.pt>
2017-06-18 18:00:54 +0100

    add database tables

---
+++ Server/Database/dbo/Tables/Image.sql
@@ -0,0 +1,11 @@
+﻿create table dbo.[Image](
+	figureId bigint not null,
+	boardId bigint not null,
+	initPointId bigint not null,
+	src nvarchar(MAX) not null,
+	width int not null,
+	height int not null,
+	constraint pk_image primary key (figureId, boardId),
+	constraint fk_image_figure foreign key (figureId, boardId) references dbo.Figure(id, boardId),
+	constraint fk_image_point foreign key (initPointId) references dbo.Point(id)
+)
