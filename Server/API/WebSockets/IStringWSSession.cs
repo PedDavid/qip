d07a0a851701881a8dadd54a62b80822196065df
Pedro Cipriano <a41890@alunos.isel.pt>
2017-06-19 12:23:57 +0100

    add interface and implementation of abstration of websocket this allows use websocket without deal with byte[]

---
+++ Server/API/WebSockets/IStringWSSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Threading;
+
+namespace API.WebSockets {
+    public interface IStringWSSession {
+        long Id { get; }
+
+        Task BroadcastAsync(string message);
+
+        void Exit();
+    }
+}
---
+++ Server/API/WebSockets/StringWebSocket.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading;
+
+namespace API.WebSockets {
+    public class StringWebSocket : IDisposable {
+        public WebSocket InternalWebSocket { get; }
+
+        public StringWebSocket(WebSocket webSocket) {
+            InternalWebSocket = webSocket;
+        }
+
+        public WebSocketCloseStatus? CloseStatus {
+            get {
+                return InternalWebSocket.CloseStatus;
+            }
+        }
+
+        public string CloseStatusDescription {
+            get {
+                return InternalWebSocket.CloseStatusDescription;
+            }
+        }
+
+        public WebSocketState State {
+            get {
+                return InternalWebSocket.State;
+            }
+        }
+
+        public string SubProtocol {
+            get {
+                return InternalWebSocket.SubProtocol;
+            }
+        }
+
+        public void Abort() {
+            InternalWebSocket.Abort();
+        }
+
+        public Task CloseAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken) {
+            return InternalWebSocket.CloseAsync(closeStatus, statusDescription, cancellationToken);
+        }
+
+        public Task CloseOutputAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken) {
+            return InternalWebSocket.CloseOutputAsync(closeStatus, statusDescription, cancellationToken);
+        }
+
+        public void Dispose() {
+            InternalWebSocket.Dispose();
+        }
+
+        public async Task<string> ReceiveAsync() {
+            WebSocketReceiveResult result;
+            byte[] buffer = new byte[4 * 1024];
+            StringBuilder builder = new StringBuilder();
+
+            do {
+                result = await InternalWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if(result.CloseStatus.HasValue)
+                    Console.WriteLine("ENdPoint");
+
+                builder.Append(Encoding.UTF8.GetString(buffer, 0, result.Count));
+
+            } while(!result.EndOfMessage && !result.CloseStatus.HasValue);
+
+            return builder.ToString();
+        }
+
+        public async Task SendAsync(string message) {
+            byte[] buffer = Encoding.UTF8.GetBytes(message);//TODO Não sei se posso fazer isto, mas ao que parece funciona
+
+            await InternalWebSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+}
