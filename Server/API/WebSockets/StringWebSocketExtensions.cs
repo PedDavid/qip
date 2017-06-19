be8985d46c79d763f6ea939f851aceb819f6adfa
Pedro Cipriano <a41890@alunos.isel.pt>
2017-06-19 12:24:40 +0100

    add extension for create stringwebsocket on request websocket

---
+++ Server/API/WebSockets/StringWebSocketExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace API.WebSockets {
+    public static class StringWebSocketExtensions {
+        public static async Task<StringWebSocket> AcceptStringWebSocketAsync(this WebSocketManager webSocketManager) {
+            WebSocket webSocket = await webSocketManager.AcceptWebSocketAsync();
+
+            return new StringWebSocket(webSocket);
+        }
+    }
+}
