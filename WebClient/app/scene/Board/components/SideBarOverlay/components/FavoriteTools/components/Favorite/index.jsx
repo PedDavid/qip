e56e95bebc9aa616f6f8aa54bc36562fc0018c22
simaovii <simaommv@gmail.com>
2017-06-19 21:01:27 +0100

    Add folowing features to favorites: -> "Remove Favorite" button -> Size of pen/eraser in favorites

--- WebClient/app/scene/Board/components/SideBarOverlay/components/FavoriteTools/components/Favorite/index.jsx
+++ WebClient/app/scene/Board/components/SideBarOverlay/components/FavoriteTools/components/Favorite/index.jsx
@@ -3,14 +3,26 @@ import styles from './styles.scss'
 import Pen from './../../../../../../../../model/tools/Pen'
 import Eraser from './../../../../../../../../model/tools/Eraser'
 
-import { Button } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 
 export default class Favorite extends React.Component {
+  state = {
+    openFavMenu: false
+  }
+  toggleFavMenu = (evt) => {
+    evt.preventDefault()
+    this.setState({openFavMenu: !this.state.openFavMenu})
+  }
+  removeFavorite = () => {
+    const fav = this.props.fav
+    this.props.removeFavorite(fav)
+  }
   changeCurrentTool = () => {
     const fav = this.props.fav
     this.props.changeCurrentTool(fav)
   }
   render () {
+    const favMenuWidth = this.state.openFavMenu ? 'visible' : 'hidden'
     const fav = this.props.fav
     // todo: change the way this is done
     let name = null
@@ -22,9 +34,13 @@ export default class Favorite extends React.Component {
       name = 'eraser'
     }
     return (
-      <div >
-        {/* it could be color:red instead of style:{color:'red'}} but the first one does not support rgba */}
-        <Button circular icon={{name, style: {color}}} className={styles.fav} onClick={this.changeCurrentTool} />
+      <div onContextMenu={this.toggleFavMenu}>
+        <Button circular className={styles.fav} onClick={this.changeCurrentTool}>
+          {/* it could be color:red instead of style:{color:'red'}} but the first one does not support rgba */}
+          <Icon className={styles.iconStyle + ' large'} name={name} style={{color: color}} />
+          <font className={styles.fontStyle} size='1'> {fav.width} </font>
+        </Button>
+        <Button onClick={this.removeFavorite} icon={{name: 'trash', style: {color: 'red'}}} content='Remove' style={{marginLeft: '-25px', visibility: favMenuWidth}} className={styles.favMenu} />
       </div>
     )
   }
