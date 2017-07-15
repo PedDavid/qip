8a7aadda7a3927ec755ef5612f3ae9cf1bb9e47f
PedDavid <peddavid96@outlook.com>
2017-07-15 15:51:58 +0100

    Change wrong endpoint, TODO for configuration
    Usage of document.location.hostname was wrong (this is the hostname
    location of the webclient server, not the api/websockets server).
    
    All these values should be easy to reconfigure and together in some kind
    of configuration file. Added TODO for this purpose.

--- WebClient/app/util/Persist.js
+++ WebClient/app/util/Persist.js
@@ -16,9 +16,10 @@ export class Persist {
   }
 
   connect = function (boardId) {
+    // TODO(peddavid): Configuration of endpoints
     const scheme = document.location.protocol === 'https:' ? 'wss' : 'ws'
     const port = document.location.port ? (57059) : ''
-    const connectionUrl = `${scheme}://${document.location.hostname}:${port}/api/websockets?id=${boardId}`
+    const connectionUrl = `${scheme}://localhost:${port}/api/websockets?id=${boardId}`
     console.log('making web socket connection to: ' + connectionUrl)
     this.socket = new WebSocket(connectionUrl)
 
