e3cfe5bb574077544424b60cf42b78e2817134a7
Pedro Cipriano <a41890@alunos.isel.pt>
2017-08-28 04:21:03 +0100

    update database to adapt to the User of the Auth0

---
+++ Server/Database/dbo/Stored Procedures/dbo.InsertOrUpdatePreferences.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[InsertOrUpdatePreferences]
+	@userId VARCHAR(128),
+	@favorites varchar(max),
+	@penColors varchar(max),
+	@created BIT OUT
+AS
+	begin try
+		set transaction isolation level serializable
+		begin TRAN
+			IF(EXISTS(SELECT * FROM dbo.Preferences WHERE id = @userId))
+			begin
+				UPDATE dbo.Preferences SET favorites = @favorites, penColors = @penColors WHERE id = @userId
+				SET @created = 0
+			END
+			ELSE
+			BEGIN
+				INSERT INTO dbo.Preferences(id, favorites, penColors) VALUES(@userId, @favorites, @penColors)
+				SET @created = 1
+			END
+		COMMIT
+	end try
+	begin catch
+		if @@TRANCOUNT <> 0
+			rollback;
+		throw
+	end catch
--- Server/Database/dbo/Tables/User_Board.sql
+++ Server/Database/dbo/Tables/User_Board.sql
@@ -1,9 +1,8 @@
 ﻿create table dbo.User_Board(
-	userId bigint not null,
+	userId varchar(128) not null, -- o length pode não ser o mais adequado, não existe um valor documentado para se usar (o max não dá para ser pk)
 	boardId bigint not null,
 	permission tinyint default 0 not null, -- 2->owner / 1->edit / 0->view
 	constraint pk_userBoard primary key (userId, boardId),
 	constraint ck_userBoard_permission check (permission=0 or permission=1 or permission=2),
-	constraint fk_userBoard_user foreign key (userId) references dbo.[User](id),
 	constraint fk_userBoard_board foreign key (boardId) references dbo.Board(id)
 )
