9dd102111359e067224c60f9070a8d4006c4f8e4
Pedro Cipriano <a41890@alunos.isel.pt>
2017-09-08 18:43:27 +0100

    remove trash

--- Server/API.Interfaces/IRepositories/IDefaultSearchableRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace API.Interfaces.IRepositories {
-    public interface IDefaultSearchableRepository<T> : IDefaultRepository<T> {
-        Task<IEnumerable<T>> GetAllAsync(long index, long size, string search);
-    }
-}
