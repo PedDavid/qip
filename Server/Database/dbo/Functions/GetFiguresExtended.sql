f3a44e6ddcbe976d0dab63b3e00454ad87df7393
Pedro Cipriano <a41890@alunos.isel.pt>
2017-09-17 16:41:46 +0100

    fix bug on functions GetImages, GetLinesInfo, GetLinesPoints, GetFigures and GetFiguresExtended

--- Server/Database/dbo/Functions/GetFiguresExtended.sql
+++ Server/Database/dbo/Functions/GetFiguresExtended.sql
@@ -5,18 +5,18 @@ as
 			img.initPointId, img.src, img.width as imageWidth, img.height as imageHeight
 			from dbo.Figure as fig 
 				full outer join dbo.[Image] as img
-					on(img.figureId = fig.id and img.boardId = @boardId)
+					on(img.figureId = fig.id and img.boardId = fig.boardId)
 				full outer join 
 				(select lin.figureId, lin.boardId, lin.isClosedForm, fStyle.color, point.x as linePointX, point.y as linePointY, (SELECT JSON_VALUE(figP.pointStyle, '$.width') as int) as pointWidth 
 					from dbo.Line as lin
 					inner join dbo.LineStyle as fStyle
 						on (fStyle.lineStyleId = lin.lineStyleId)
 					inner join dbo.Line_Point as figP
-						on (figP.figureId = lin.figureId and figP.boardId = @boardId)
+						on (figP.figureId = lin.figureId and figP.boardId = lin.boardId)
 							inner join dbo.Point as point
 								on(figP.pointId = point.id)
 						
 				) as lineX
-				on(lineX.figureId = fig.id and lineX.boardId = @boardId)
+				on(lineX.figureId = fig.id and lineX.boardId = fig.boardId)
 			where (fig.boardId = @boardId)
 	)
--- Server/Database/dbo/Functions/GetLinesInfo.sql
+++ Server/Database/dbo/Functions/GetLinesInfo.sql
@@ -5,7 +5,7 @@ AS
 		select fig.boardId, fig.id, lin.isClosedForm, fStyle.lineStyleId, fStyle.color as lineColor
 			from dbo.Figure as fig 
 				inner join dbo.Line as lin
-					on(lin.boardId=@boardId and lin.figureId = fig.id)
+					on(lin.boardId=fig.boardId and lin.figureId = fig.id)
 				inner join dbo.LineStyle as fStyle
 					on (fStyle.lineStyleId = lin.lineStyleId)
 			where (fig.boardId = @boardId)
