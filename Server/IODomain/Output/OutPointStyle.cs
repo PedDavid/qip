429b28b3b5c55ce4823dc6ec5d2448564e6721d9
Pedro Cipriano <a41890@alunos.isel.pt>
2017-09-18 14:52:35 +0100

    replace json PointStyle to table PointStyle

--- Server/IODomain/Output/OutPointStyle.cs
+++ Server/IODomain/Output/OutPointStyle.cs
@@ -1,5 +1,5 @@
 ﻿namespace QIP.IODomain.Output {
     public class OutPointStyle {
-        public int Width { get; set; }
+        public int? Width { get; set; }//TODO verificar tudo para tirar o nullable
     }
 }
---
+++ Server/Repositories/Extensions/SqlInt32Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlTypes;
+
+namespace QIP.Repositories.Extensions {
+    public static class SqlInt32Extensions {
+        //Converte o SqlInt32 em int?, sendo o SqlInt32.Null equivalente a null
+        public static int? ToNullableInt(this SqlInt32 sqlInt) {
+            return sqlInt.IsNull ? null : (int?)sqlInt.Value;
+        }
+    }
+}
